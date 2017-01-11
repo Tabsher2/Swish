ae1ae7ba80d054a22a04f4e5b5362d3fa1e0f271
Tabsher2 <absher.trevor@gmail.com>
2017-01-11 16:50:07 -0500

    Moved Files to Folder
    Created JSON Services Folder

---
+++ JSON Services/BoilerPlate.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.IO;
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class JSONServices_BoilerPlate : System.Web.UI.Page
+{
+
+	public struct GenericRequest
+	{
+		public int value;
+		public string text;
+	}
+	
+	public struct GenericResponse
+	{
+		public int value;
+		public string text;
+		public string error;
+	}
+
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		GenericRequest request;
+		GenericResponse response = new GenericResponse();
+		response.error = String.Empty;
+
+		// Need passed in store id and number of requested results.
+		// 1. Deserialize the incoming Json.
+		try
+		{
+			request = GetRequestInfo();
+		}
+		catch (Exception ex)
+		{
+			response.error = ex.Message.ToString();
+
+			// Return the results as Json.
+			SendInfoAsJson(response);
+
+			return;
+		}
+
+		// Do stuff here.
+		SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+		try
+		{
+			connection.Open();
+
+
+
+		}
+		catch (Exception ex)
+		{
+			response.error = ex.Message.ToString();
+		}
+		finally
+		{
+			if (connection.State == ConnectionState.Open)
+			{
+				connection.Close();
+			}
+		}
+
+		SendInfoAsJson(response);
+	}
+
+	GenericRequest GetRequestInfo()
+	{
+		// Get the Json from the POST.
+		string strJson = String.Empty;
+		HttpContext context = HttpContext.Current;
+		context.Request.InputStream.Position = 0;
+		using (StreamReader inputStream = new StreamReader(context.Request.InputStream))
+		{
+			strJson = inputStream.ReadToEnd();
+		}
+
+		// Deserialize the Json.
+		GenericRequest request = JsonConvert.DeserializeObject<GenericRequest>(strJson);
+
+		return (request);
+	}
+
+	void SendInfoAsJson(GenericResponse response)
+	{
+		string strJson = JsonConvert.SerializeObject(response);
+		Response.ContentType = "application/json; charset=utf-8";
+		Response.Write(strJson);
+		Response.End();
+	}
+
+}
