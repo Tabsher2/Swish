65ea67fef186afab8c93a589a914a512cdb1ed79
Tabsher2 <absher.trevor@gmail.com>
2017-01-18 15:57:03 -0500

    Added Basic Unity Files
    Added Game Scene and Controller Script

---
+++ Swish/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across XY plane
+            transform.Translate(-touchDeltaPosition.x, -touchDeltaPosition.y, 0);
+        }
+    }
+}
