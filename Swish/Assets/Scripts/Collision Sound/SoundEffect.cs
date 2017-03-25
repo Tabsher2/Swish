1d41c756e698663081dff3500335ed1495155629
YacineT <enicay23@yahoo.com>
2017-03-25 19:27:49 -0400

    Sounds

---
+++ Swish/Assets/Scripts/Collision Sound/SoundEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour {
+
+	public AudioClip clip;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		AudioSource.PlayClipAtPoint(clip, transform.position);
+	}
+}
