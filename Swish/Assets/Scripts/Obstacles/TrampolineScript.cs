bb2a922519b67abc6291b73ee1ac2ac6bacc6b39
YacineT <enicay23@yahoo.com>
2017-03-25 19:26:57 -0400

    Signed-off-by: YacineT <enicay23@yahoo.com>

--- Swish/Assets/Scripts/Obstacles/TrampolineScript.cs
+++ Swish/Assets/Scripts/Obstacles/TrampolineScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrampolineScript : MonoBehaviour {
+	public AudioClip clip;
 
 	// Use this for initialization
 	void Start () {
@@ -17,5 +18,6 @@ public class TrampolineScript : MonoBehaviour {
     private void OnCollisionEnter(Collision collision)
     {
         collision.rigidbody.velocity = new Vector3(collision.rigidbody.velocity.x, collision.rigidbody.velocity.y*1.5f, collision.rigidbody.velocity.z);
+		AudioSource.PlayClipAtPoint(clip, transform.position);
     }
 }
