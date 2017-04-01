ab56bbaac3264925312f73a321da5270674d7fe3
YacineT <enicay23@yahoo.com>
2017-04-01 19:17:39 -0400

    Added New script for Grass and Court for rolling audio

---
+++ Swish/Assets/Scripts/Collision Sound/GrassSoundEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassSoundEffect : MonoBehaviour {
+
+	public AudioClip clip1;
+	public AudioClip clip2;
+	private AudioSource audio;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		bool played = false;
+		audio = GetComponent<AudioSource> ();
+		audio.clip = clip1;
+
+		audio.Play();
+	}
+
+	private void OnCollisionStay(Collision collision)
+	{
+		audio = GetComponent<AudioSource> ();
+		audio.clip = clip2;
+		if (!audio.isPlaying) {
+			audio.Play ();
+		}
+	}
+}
