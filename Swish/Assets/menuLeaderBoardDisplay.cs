b139dc0511c943702411ce9ba908c5220884beed
Mike <mslater715@gmail.com>
2017-04-02 15:12:42 -0400

    Leaderboard menu panel created

---
+++ Swish/Assets/menuLeaderBoardDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuLeaderBoardDisplay : MonoBehaviour {
+
+    public Text leaderTable;
+    string tableInfo;
+    string[] names = new string[10];
+    int[] wins = new int[10];
+
+	// Use this for initialization
+	void Start () {
+
+        for( int  i = 0; i < 10; i++)
+        {
+            //retrieve users in rank order from database and store here
+            names[i] = "user" + (i+1);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            //retrieve each users wins and store here
+        }
+
+        tableInfo = "Rank\t\t\t" + "User\t\t\t" + "Wins\n";
+        for (int i = 0; i < 10; i++)
+        {
+            tableInfo += (i+1) + "\t\t\t" +  names[i] + "\t\t\t" + wins[i] + "\n";
+        }
+        leaderTable.text = tableInfo;
+	}
+	
+}
