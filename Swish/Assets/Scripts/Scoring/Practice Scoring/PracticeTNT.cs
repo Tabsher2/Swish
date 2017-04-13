7406838933d46860f323b77f5297f7745e4f6348
YacineT <enicay23@yahoo.com>
2017-04-12 21:27:00 -0400

    Fixed Tree Audio,Looped Menu Music,Added Net Noise

--- Swish/Assets/Scripts/Scoring/Practice Scoring/PracticeTNT.cs
+++ Swish/Assets/Scripts/Scoring/Practice Scoring/PracticeTNT.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PracticeTNT : MonoBehaviour
-{
-
+{	
+	private AudioSource audio;
     private static bool trigger;
-
+	public AudioClip clip;
+	
     private void OnTriggerEnter(Collider other)
     {
         if (!PracticeBNT.isTriggered())
         {
             //SHOT MADE HERE
+			audio = GetComponent<AudioSource> ();
+			audio.Play();
             trigger = true;
         }
         //Debug.Log(trigger + "top");
